9ea68cf87d7eeaa954b1abb4e9c48ebcd347237a
Murad Rustamov <mrustamov@studenten.hs-bremerhaven.de>
2020-12-23 22:54:57 +0100

    Life U05 / Ready to manipulation

--- v_2/A05/Life.java
+++ v_2/A05/Life.java
@@ -6,6 +6,18 @@ public class Life {
         boolean [][] array  = new boolean[len][len];
 
         randFill(array);
+
+        for(int y = 0; y < len; ++y) {
+            for(int x = 0; x < len; ++x) {
+                for(int i = (y>0?y-1:y); i <= (y<len-1?y+1:y); ++i) {
+                    for(int k = (x>0?x-1:x); k <= (x<len-1?x+1:x); ++k) {
+                        if(i != y && k != x && array[i][k])
+                            System.out.println("ishleyir.");
+                    }
+                }
+            }
+        }
+
         print(array);
     }
 
